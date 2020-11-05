5c578fa75d1040d7eda38a3ad3aa84e3e3c3c929
martreyz <martreyz@gmail.com>
2020-11-05 13:42:59 +0100

    Add preview props

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import '../stylesheets/App.scss';
-import Header from './Header';
-import Preview from './Preview';
-import Forms from './Forms/Forms';
-import Footer from './Footer';
+import React from "react";
+import "../stylesheets/App.scss";
+import Header from "./Header";
+import Preview from "./Preview";
+import Forms from "./Forms/Forms";
+import Footer from "./Footer";
+import Photo from "../images/totoro.jpg";
+
+const data = {
+  name: "Ada Lovelace",
+  job: "Programadora",
+  photo: Photo,
+  phone: "666 66 66 66",
+  mail: "ada.lovelace@ada.com",
+  linkedin: "adita.lovelace",
+  github: "adita.lovelace",
+};
 
 class App extends React.Component {
   // constructor(props) {
@@ -15,7 +26,15 @@ class App extends React.Component {
       <>
         <Header />
         <main className="main-aplication">
-          <Preview />
+          <Preview
+            name={data.name}
+            job={data.job}
+            photo={data.photo}
+            phone={data.phone}
+            linkedin={data.linkedin}
+            github={data.github}
+            mail={data.mail}
+          />
           <Forms />
         </main>
         <Footer />
--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import '../stylesheets/layout/_main-preview.scss';
+import React from "react";
+import "../stylesheets/layout/_main-preview.scss";
 
 class Preview extends React.Component {
   render() {
+    const pic = this.props.photo;
+
     return (
       <section className="preview">
         <div className="preview-button-div">
@@ -13,29 +15,52 @@ class Preview extends React.Component {
         </div>
         <div className="preview__card js-preview__card">
           <div className="preview__card--text js-preview__card--text">
-            <h3 className="text-name js__preview-name js-text-name">Nombre Apellido</h3>
-            <h4 className="text-work js__preview-job js-text-work">Front-end developer</h4>
+            <h3 className="text-name js__preview-name js-text-name">
+              {this.props.name}
+            </h3>
+            <h4 className="text-work js__preview-job js-text-work">
+              {this.props.job}
+            </h4>
           </div>
-          <div className="profile__image preview__card--img js__profile-image"></div>
+          <div
+            style={{ backgroundImage: `url(${pic})` }}
+            className="profile__image preview__card--img js__profile-image"
+          ></div>
           <nav>
             <ul className="preview__card--nav">
               <li>
-                <a href="/" className="icons js__preview-phone js-icons" target="blank">
+                <a
+                  href={this.props.phone}
+                  className="icons js__preview-phone js-icons"
+                  target="blank"
+                >
                   <i className="fas fa-mobile-alt"></i>
                 </a>
               </li>
               <li>
-                <a href="/" className="icons js__preview-mail js-icons" target="blank">
+                <a
+                  href={`mailto:${this.props.mail}`}
+                  className="icons js__preview-mail js-icons"
+                  target="blank"
+                >
                   <i className="far fa-envelope"></i>
                 </a>
               </li>
               <li>
-                <a href="/" className="icons js__preview-linkedin js-icons" target="blank">
+                <a
+                  href={this.props.linkedin}
+                  className="icons js__preview-linkedin js-icons"
+                  target="blank"
+                >
                   <i className="fab fa-linkedin-in"></i>
                 </a>
               </li>
               <li>
-                <a href="/" className="icons js__preview-github js-icons" target="blank">
+                <a
+                  href={this.props.github}
+                  className="icons js__preview-github js-icons"
+                  target="blank"
+                >
                   <i className="fab fa-github-alt"></i>
                 </a>
               </li>
