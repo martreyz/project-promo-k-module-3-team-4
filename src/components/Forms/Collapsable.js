d7f134ae1d61809c2d2c7d8d71a67eb7ab18d681
martreyz <martreyz@gmail.com>
2020-11-10 18:21:35 +0100

    Unify collapsable styles

--- src/components/Forms/Collapsable.js
+++ src/components/Forms/Collapsable.js
@@ -5,32 +5,32 @@ class Collapsable extends React.Component {
     super(props);
     this.handleClick = this.handleClick.bind(this);
     this.state = {
-      isClosed: true
-    }
+      isClosed: true,
+    };
   }
   handleClick() {
     this.setState({
-      isClosed: !this.state.isClosed
-    })
+      isClosed: !this.state.isClosed,
+    });
     console.log(this.state.isClosed);
   }
   render() {
     const openClassName = this.state.isClosed ? "js-design__form" : "";
     return (
       //"share js-share"
-      <>
-        <div className="share__title js-collapsable-title" onClick={this.handleClick}>
-          <div className="share__title--container">
+      <section className="borderContainer">
+        <div
+          className="collapsable__title js-collapsable-title"
+          onClick={this.handleClick}
+        >
+          <div className="collapsable__title--container">
             <i className="fas fa-share-alt container-share"></i>
             <h2 className="container-text">{this.props.name}</h2>
           </div>
-          <i className="fas fa-chevron-up js-fa-chevron-up share__title--arrow"></i>
+          <i className="fas fa-chevron-up js-fa-chevron-up collapsable__title--arrow"></i>
         </div>
-        <div className={openClassName}>
-          {this.props.children}
-        </div>
-      </>
-
+        <div className={openClassName}>{this.props.children}</div>
+      </section>
     );
   }
 }
